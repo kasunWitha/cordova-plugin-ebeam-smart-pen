2701f47355c472e3f9c1d76468a861b7a87d8834
kasunWitha <kaswith4n@gmail.com>
2018-10-02 11:37:40 +0530

    bla bla

--- www/EbeamSmartpen.js
+++ www/EbeamSmartpen.js
@@ -2,10 +2,10 @@ var exec = require('cordova/exec');
 
 
 
-module.exports.create = function(success, error, arg0){
+module.exports.create = function(arg0, success, error){
     exec(success, error, 'EbeamSmartpen', 'create', [arg0]);
 }
 
-module.exports.isPenMode = function(success, error, arg0){
+module.exports.isPenMode = function(arg0, success, error){
     exec(success, error, 'EbeamSmartpen', 'isPenMode', [arg0]);
 }
